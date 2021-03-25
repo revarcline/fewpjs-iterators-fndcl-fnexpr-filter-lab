e54ddb77a88ffed7646f5de2edde9740220b5448
Alex Cline <rev.a.r.cline@gmail.com>
2021-03-25 17:37:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+  // case insensitive
+  return drivers.filter((driver) => {
+    return driver.toLowerCase() === string.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, string) {
+  // beginning letters
+  return drivers.filter((driver) => {
+    return driver.slice(0, string.length) === string;
+  });
+}
+
+function matchName(drivers, string) {
+  // checks for exact name
+  return drivers.filter((driver) => {
+    return driver.name === string;
+  });
+}
